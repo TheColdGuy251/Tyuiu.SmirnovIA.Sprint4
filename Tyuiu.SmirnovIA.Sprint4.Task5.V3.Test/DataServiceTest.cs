a8a2cf5974a1744d92d51e6b48b52b5801f46946
TheColdGuy251 <smirnovilya7031@gmail.com>
2023-11-12 16:58:47 +0500

    Task5

---
+++ Tyuiu.SmirnovIA.Sprint4.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.SmirnovIA.Sprint4.Task5.V3.Lib;
+
+namespace Tyuiu.SmirnovIA.Sprint4.Task5.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Valid()
+        {
+            DataService ds = new DataService();
+            int[,] mas2 = new int[5, 5] { { -5, 7, 7, 8, 5 },
+                                          { 6, 5, 6, 8, 6 },
+                                          { 7, 6, -8, 8, -5 },
+                                          { 7, 6, 7, 8, 6 },
+                                          { -7, 6, 7, 7, 5 }};
+            int[,] res = ds.Calculate(mas2);
+            int[,] wait = new int[5, 5] { { 0, 7, 7, 8, 5 },
+                                          { 6, 5, 6, 8, 6 },
+                                          { 7, 6, 0, 8, 0 },
+                                          { 7, 6, 7, 8, 6 },
+                                          { 0, 6, 7, 7, 5 }};
+            CollectionAssert.AreEqual(mas2, res);
+        }
+    }
+}
---
+++ Tyuiu.SmirnovIA.Sprint4.Task5.V3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SmirnovIA.Sprint4.Task5.V3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SmirnovIA.Sprint4.Task5.V3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a67ec284-e7b8-4478-b40b-d8b293df2155")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
